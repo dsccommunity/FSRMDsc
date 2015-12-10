ee2c34109da71a93236181de3d4d27cd6ed5570d
Daniel Scott-Raynsford <plagueho@gmail.com>
2015-12-10 16:13:50 +1300

    cFSRMFileScreenAction Integration Tests added.

---
+++ Tests/Integration/BMD_cFSRMFileScreenAction.Integration.Tests.ps1
@@ -0,0 +1,70 @@
+$DSCModuleName      = 'cFSRM'
+$DSCResourceName    = 'BMD_cFSRMFileScreenAction'
+
+#region HEADER
+if ( (-not (Test-Path -Path '.\DSCResource.Tests\')) -or `
+     (-not (Test-Path -Path '.\DSCResource.Tests\TestHelper.psm1')) )
+{
+    & git @('clone','https://github.com/PlagueHO/DscResource.Tests.git')
+}
+Import-Module .\DSCResource.Tests\TestHelper.psm1 -Force
+$TestEnvironment = Initialize-TestEnvironment `
+    -DSCModuleName $DSCModuleName `
+    -DSCResourceName $DSCResourceName `
+    -TestType Integration 
+#endregion
+
+# Using try/finally to always cleanup even if something awful happens.
+try
+{
+    #region Integration Tests
+    $ConfigFile = Join-Path -Path $PSScriptRoot -ChildPath "$DSCResourceName.config.ps1"
+    . $ConfigFile
+
+    Describe "$($DSCResourceName)_Integration" {
+        # Create the File Screen that will be worked with 
+        New-FSRMFileScreen `
+            -Path $fileScreen.Path `
+            -Description $fileScreen.Description `
+            -Active $fileScreen.Active `
+            -IncludeGroup $fileScreen.IncludeGroup `
+            -Template $fileScreen.Template `
+            -MatchesTemplate $fileScreen.MatchesTemplate 
+            
+        #region DEFAULT TESTS
+        It 'Should compile without throwing' {
+            {
+                Invoke-Expression -Command "$($DSCResourceName)_Config -OutputPath `$TestEnvironment.WorkingFolder"
+                Start-DscConfiguration -Path $TestEnvironment.WorkingFolder -ComputerName localhost -Wait -Verbose -Force
+            } | Should not throw
+        }
+
+        It 'should be able to call Get-DscConfiguration without throwing' {
+            { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should Not throw
+        }
+        #endregion
+
+        It 'Should have set the resource and all the parameters should match' {
+            # Get the Rule details
+            $fileScreenNew = Get-FSRMFileScreen -Path $fileScreen.Path
+            $fileScreenAction.Type               | Should Be $fileScreenNew.Actions[0].Type
+            $fileScreenAction.Subject            | Should Be $fileScreenNew.Actions[0].Subject
+            $fileScreenAction.Body               | Should Be $fileScreenNew.Actions[0].Body
+            $fileScreenAction.MailBCC            | Should Be $fileScreenNew.Actions[0].MailBCC
+            $fileScreenAction.MailCC             | Should Be $fileScreenNew.Actions[0].MailCC
+            $fileScreenAction.MailTo             | Should Be $fileScreenNew.Actions[0].MailTo
+        }
+        
+        # Clean up
+        Remove-FSRMFileScreen `
+            -Path $fileScreen.Path `
+            -Confirm:$false
+    }
+    #endregion
+}
+finally
+{
+    #region FOOTER
+    Restore-TestEnvironment -TestEnvironment $TestEnvironment
+    #endregion
+}
