2fc9ef36c93b83dc5679f08b752c5ad85185e2dd
Daniel Scott-Raynsford <plagueho@gmail.com>
2020-06-14 15:35:40 +1200

    Fix integration tests

--- tests/Integration/DSC_FSRMClassificationRule.Tests.ps1
+++ tests/Integration/DSC_FSRMClassificationRule.Tests.ps1
@@ -1,82 +1,109 @@
-$script:DSCModuleName      = 'FSRMDsc'
-$script:DSCResourceName    = 'DSC_FSRMclassificationRule'
+$script:dscModuleName = 'FSRMDsc'
+$script:dscResourceName = 'DSC_FSRMclassificationRule'
 
-#region HEADER
-# Integration Test Template Version: 1.1.1
-[System.String] $script:moduleRoot = Split-Path -Parent (Split-Path -Parent $PSScriptRoot)
-
-if ( (-not (Test-Path -Path (Join-Path -Path $script:moduleRoot -ChildPath 'DSCResource.Tests'))) -or `
-     (-not (Test-Path -Path (Join-Path -Path $script:moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1'))) )
-{
-    & git @('clone','https://github.com/PowerShell/DscResource.Tests.git',(Join-Path -Path $script:moduleRoot -ChildPath '\DSCResource.Tests\'))
-}
-
-Import-Module (Join-Path -Path $script:moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1') -Force
-Import-Module (Join-Path -Path $script:moduleRoot -ChildPath "$($script:DSCModuleName).psd1") -Force
-$TestEnvironment = Initialize-TestEnvironment `
-    -DSCModuleName $script:DSCModuleName `
-    -DSCResourceName $script:DSCResourceName `
-    -TestType Integration
-#endregion
-
-# Using try/finally to always cleanup even if something awful happens.
 try
 {
-    #region Integration Tests
-    $ConfigFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:DSCResourceName).config.ps1"
-    . $ConfigFile
+    Import-Module -Name DscResource.Test -Force -ErrorAction 'Stop'
+}
+catch [System.IO.FileNotFoundException]
+{
+    throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -Tasks build" first.'
+}
 
-    Describe "$($script:DSCResourceName)_Integration" {
-        # Create the Classification Property that will be worked with
-        New-FSRMClassificationPropertyDefinition `
-            -Name $classificationProperty.Name `
-            -Type $classificationProperty.Type `
-            -PossibleValue @(New-FSRMClassificationPropertyValue -Name $classificationProperty.PossibleValue[0])
+$script:testEnvironment = Initialize-TestEnvironment `
+    -DSCModuleName $script:dscModuleName `
+    -DSCResourceName $script:dscResourceName `
+    -ResourceType 'Mof' `
+    -TestType 'Integration'
 
-        #region DEFAULT TESTS
-        It 'Should compile and apply the MOF without throwing' {
-            {
-                & "$($script:DSCResourceName)_Config" -OutputPath $TestDrive
-                Start-DscConfiguration `
-                    -Path $TestDrive `
-                    -ComputerName localhost `
-                    -Wait `
-                    -Verbose `
-                    -Force `
-                    -ErrorAction Stop
-            } | Should -Not -Throw
+Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
+
+try
+{
+    Describe 'FSRMAutoQuota Integration Tests' {
+        $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).Config.ps1"
+        . $configFile
+
+        Describe "$($script:dscResourceName)_Integration" {
+            $configData = @{
+                AllNodes = @(
+                    @{
+                        NodeName                   = 'localhost'
+                        Name                       = 'IntegrationTest'
+                        Description                = 'Test Rule Description'
+                        ClassificationMechanism    = 'Content Classifier'
+                        ContentRegularExpression   = @( 'Regex1', 'Regex2' )
+                        ContentString              = @( 'String1', 'String2' )
+                        ContentStringCaseSensitive = @( 'String1', 'String2' )
+                        Disabled                   = $false
+                        Flags                      = @( 1024 )
+                        Namespace                  = @( '[FolderUsage_MS=User Files]', $ENV:Temp )
+                        Parameters                 = @( 'FileName=*.doc', 'FSRMClearruleInternal=0', 'FSRMClearPropertyInternal=1024' )
+                        Property                   = 'IntegrationTest'
+                        PropertyValue              = 'Value1'
+                        ReevaluateProperty         = 'Never'
+                    }
+                )
+            }
+
+            # Create the Classification Property that will be worked with
+            New-FSRMClassificationPropertyDefinition `
+                -Name $configData.AllNodes[0].Name `
+                -Type 'SingleChoice' `
+                -PossibleValue @(New-FSRMClassificationPropertyValue -Name 'Value1' )
+
+            It 'Should compile and apply the MOF without throwing' {
+                {
+                    & "$($script:dscResourceName)_Config" `
+                        -OutputPath $TestDrive `
+                        -ConfigurationData $configData
+
+                    $startDscConfigurationParameters = @{
+                        Path         = $TestDrive
+                        ComputerName = 'localhost'
+                        Wait         = $true
+                        Verbose      = $true
+                        Force        = $true
+                        ErrorAction  = 'Stop'
+                    }
+
+                    Start-DscConfiguration @startDscConfigurationParameters
+                } | Should -Not -Throw
+            }
+
+            It 'Should be able to call Get-DscConfiguration without throwing' {
+                {
+                    Get-DscConfiguration -Verbose -ErrorAction Stop
+                } | Should -Not -Throw
+            }
+
+            It 'Should have set the resource and all the parameters should match' {
+                $current = Get-DscConfiguration | Where-Object -FilterScript {
+                    $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
+                }
+                $current.Name | Should -Be  $configData.AllNodes[0].Name
+                $current.Description | Should -Be  $configData.AllNodes[0].Description
+                $current.ClassificationMechanism | Should -Be  $configData.AllNodes[0].ClassificationMechanism
+                $current.ContentRegularExpression | Should -Be  $configData.AllNodes[0].ContentRegularExpression
+                $current.ContentString | Should -Be  $configData.AllNodes[0].ContentString
+                $current.ContentStringCaseSensitive | Should -Be  $configData.AllNodes[0].ContentStringCaseSensitive
+                $current.Disabled | Should -Be  $configData.AllNodes[0].Disabled
+                $current.Flags | Should -Be  $configData.AllNodes[0].Flags
+                $current.Namespace | Should -Be  $configData.AllNodes[0].Namespace
+                $current.Parameters | Should -Be  $configData.AllNodes[0].Parameters
+                $current.Property | Should -Be  $configData.AllNodes[0].Property
+                $current.PropertyValue | Should -Be  $configData.AllNodes[0].PropertyValue
+                $current.ReevaluateProperty | Should -Be  $configData.AllNodes[0].ReevaluateProperty
+            }
+
+            # Clean up
+            Remove-FSRMClassificationRule `
+                -Name $configData.AllNodes[0].Name `
+                -Confirm:$false
+            Remove-FSRMClassificationPropertyDefinition `
+                -Name $configData.AllNodes[0].Name `
+                -Confirm:$false
         }
-
-        It 'Should be able to call Get-DscConfiguration without throwing' {
-            { Get-DscConfiguration -Verbose -ErrorAction Stop } | Should -Not -throw
-        }
-        #endregion
-
-        It 'Should have set the resource and all the parameters should match' {
-            # Get the Rule details
-            $classificationRuleNew = Get-FSRMclassificationRule -Name $classificationRule.Name
-            $classificationRule.Name                       | Should -Be  $classificationRuleNew.Name
-            $classificationRule.Description                | Should -Be  $classificationRuleNew.Description
-            $classificationRule.ClassificationMechanism    | Should -Be  $classificationRuleNew.ClassificationMechanism
-            $classificationRule.ContentRegularExpression   | Should -Be  $classificationRuleNew.ContentRegularExpression
-            $classificationRule.ContentString              | Should -Be  $classificationRuleNew.ContentString
-            $classificationRule.ContentStringCaseSensitive | Should -Be  $classificationRuleNew.ContentStringCaseSensitive
-            $classificationRule.Disabled                   | Should -Be  $classificationRuleNew.Disabled
-            $classificationRule.Flags                      | Should -Be  $classificationRuleNew.Flags
-            $classificationRule.Namespace                  | Should -Be  $classificationRuleNew.Namespace
-            $classificationRule.Parameters                 | Should -Be  $classificationRuleNew.Parameters
-            $classificationRule.Property                   | Should -Be  $classificationRuleNew.Property
-            $classificationRule.PropertyValue              | Should -Be  $classificationRuleNew.PropertyValue
-            $classificationRule.ReevaluateProperty         | Should -Be  $classificationRuleNew.ReevaluateProperty
-        }
-
-        # Clean up
-        Remove-FSRMClassificationRule `
-            -Name $classificationRule.Name `
-            -Confirm:$false
-        Remove-FSRMClassificationPropertyDefinition `
-            -Name $classificationProperty.Name `
-            -Confirm:$false
     }
 }
 finally
--- tests/Integration/DSC_FSRMClassificationRule.config.ps1
+++ tests/Integration/DSC_FSRMClassificationRule.config.ps1
@@ -1,41 +1,21 @@
-$classificationProperty = @{
-    Name = 'IntegrationTest'
-    Type = 'SingleChoice'
-    PossibleValue = @( 'Value1' )
-}
-$classificationRule = @{
-    Name                       = 'IntegrationTest'
-    Description                = 'Test Rule Description'
-    ClassificationMechanism    = 'Content Classifier'
-    ContentRegularExpression   = @( 'Regex1','Regex2' )
-    ContentString              = @( 'String1','String2' )
-    ContentStringCaseSensitive = @( 'String1','String2' )
-    Disabled                   = $false
-    Flags                      = @( 1024 )
-    Namespace                  = @( '[FolderUsage_MS=User Files]',$ENV:Temp )
-    Parameters                 = @( 'FileName=*.doc','FSRMClearruleInternal=0','FSRMClearPropertyInternal=1024' )
-    Property                   = $classificationProperty.Name
-    PropertyValue              = $classificationProperty.PossibleValue[0]
-    ReevaluateProperty         = 'Never'
-}
-
 Configuration DSC_FSRMClassificationRule_Config {
     Import-DscResource -ModuleName FSRMDsc
+
     node localhost {
        FSRMClassificationRule Integration_Test {
-            Name                       = $classificationRule.Name
-            Description                = $classificationRule.Description
-            ClassificationMechanism    = $classificationRule.ClassificationMechanism
-            ContentRegularExpression   = $classificationRule.ContentRegularExpression
-            ContentString              = $classificationRule.ContentString
-            ContentStringCaseSensitive = $classificationRule.ContentStringCaseSensitive
-            Disabled                   = $classificationRule.Disabled
-            Flags                      = $classificationRule.Flags
-            Namespace                  = $classificationRule.Namespace
-            Parameters                 = $classificationRule.Parameters
-            Property                   = $classificationRule.Property
-            PropertyValue              = $classificationRule.PropertyValue
-            ReevaluateProperty         = $classificationRule.ReevaluateProperty
+            Name                       = $Node.Name
+            Description                = $Node.Description
+            ClassificationMechanism    = $Node.ClassificationMechanism
+            ContentRegularExpression   = $Node.ContentRegularExpression
+            ContentString              = $Node.ContentString
+            ContentStringCaseSensitive = $Node.ContentStringCaseSensitive
+            Disabled                   = $Node.Disabled
+            Flags                      = $Node.Flags
+            Namespace                  = $Node.Namespace
+            Parameters                 = $Node.Parameters
+            Property                   = $Node.Property
+            PropertyValue              = $Node.PropertyValue
+            ReevaluateProperty         = $Node.ReevaluateProperty
         }
     }
 }
